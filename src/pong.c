6bd8446f3461ddbdf054fe1624a5510f75def500
Laurene Bari <laureneb@ev-r3.kzn.21-school.ru>
2023-03-11 14:59:26 +0300

    add score and 3 part of rakets

--- src/pong.c
+++ src/pong.c
@@ -3,12 +3,14 @@
 #include "view.h"
 
 int main() {
-    int x_ball, y_ball;
+    int x_ball = 39;
+    int y_ball = 14;
     int x_l_rack = 9;
     int x_r_rack = 70;
     int y_l_rack = 13;
     int y_r_rack = 13;
-    scanf("%d %d", &x_ball, &y_ball);
-    view_board(x_ball, y_ball, x_l_rack, y_l_rack, x_r_rack, y_r_rack);
+    int score_l = 0;
+    int score_r = 0;
+    view_board(x_ball, y_ball, x_l_rack, y_l_rack, x_r_rack, y_r_rack, score_l, score_r);
     return 0;
 }
--- src/view.h
+++ src/view.h
@@ -1,16 +1,19 @@
 #include <stdio.h>
 
 void wall(int width);
+int isRack(int line, int col, int x_l_rack, int y_l_rack, int x_r_rack, int y_r_rack);
 
-void view_board(int x_ball, int y_ball, int x_l_rack, int y_l_rack, int x_r_rack, int y_r_rack) {
+void view_board(int x_ball, int y_ball, int x_l_rack, int y_l_rack, int x_r_rack, int y_r_rack, int score_l, int score_r) {
     int width = 80;
     int height = 25;
+    printf("\n");
+    printf("Score %d:%d\n\n", score_l, score_r);
     for (int line = 0; line <= 25; line++) {
         if (line == 0 || line == height)
             wall(width);
         else {
             for (int col = 0; col < width; col++) {
-                if ((col == x_l_rack && line == y_l_rack) || (col == x_r_rack && line == y_r_rack))
+                if (isRack(line, col, x_l_rack, y_l_rack, x_r_rack, y_r_rack))
                     printf("|");
                 else if (col == x_ball && line == y_ball)
                     printf("*");
@@ -29,4 +32,13 @@ void wall(int width) {
     printf("\n");
 }
 
-
+int isRack(int line, int col, int x_l_rack, int y_l_rack, int x_r_rack, int y_r_rack) {
+    int res = 0;   
+    if (col == x_l_rack)
+        if (line <= y_l_rack + 2 && line >= y_l_rack)
+            res = 1;
+    if (col == x_r_rack)
+        if (line <= y_r_rack + 2 && line >= y_r_rack)
+            res = 1;
+    return res;
+}
